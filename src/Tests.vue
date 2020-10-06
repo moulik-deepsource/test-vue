f6e02e286b3b457f0862e41d692c759bc546652a
Moulik Aggarwal <moulik@deepsource.io>
2020-10-07 00:36:47 +0530

    Updated
    Signed-off-by: Moulik Aggarwal <moulik@deepsource.io>

---
+++ src/Tests.vue
@@ -0,0 +1,21 @@
+<template>
+  {{ 'Lorem ipsum' }}
+  {{ "Lorem ipsum" }}
+  {{ `Lorem ipsum` }}
+</template>
+<script>
+Vue.component('some-comp', {
+  data: function() {
+return {
+    foo: 'bar'
+  };
+}
+})
+export default {
+  data: function() {
+return {
+    foo: 'bar'
+  };
+}
+}
+</script>
