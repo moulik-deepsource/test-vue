9b9b58a6d958429f8ad43316556bc6f6ff14bd10
Moulik Aggarwal <68378241+moulik-deepsource@users.noreply.github.com>
2020-10-07 13:05:12 +0530

    Update Another.vue

--- src/Another.vue
+++ src/Another.vue
@@ -8,4 +8,16 @@
   <div :foo="'bar'"/>
 </template>
 <script>
+<script>
+Vue.component('some-comp', {
+  data: {
+    foo: 'bar'
+  }
+})
+
+export default {
+  data: {
+    foo: 'bar'
+  }
+}
 </script>
